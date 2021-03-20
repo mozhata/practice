73f8de1725d8492c30115f8d4207a9acfcfcdcb6
mozhata <zhangyongkang@tal.com>
2021-03-21 00:05:42 +0800

    位运算和半个IntSet

--- algorithm/main.go
+++ algorithm/main.go
@@ -1,1 +1,16 @@
 package main
+
+import "practice/algorithm/basic"
+
+/*
+第一阶段目标: 快速过完 刷题路径, 有不会的先跳过, 等刷完了在回过头来重点补课
+第二阶段: 照着大话数据结构刷
+			上面的算法比较简单, 可能需要照着leetcode把基础的都刷一遍 ?
+*/
+
+func main() {
+	// basic.TryDeDuplicate()
+
+	// basic.TryBitSet()
+	basic.TryIntSet()
+}
