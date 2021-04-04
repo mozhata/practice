74967442c51f444cd9f9441468899ef83ef1c534
mozhata <zhangyongkang@tal.com>
2021-04-04 20:29:46 +0800

    增加二分查找插入实现

--- algorithm/basic/search.go
+++ algorithm/basic/search.go
@@ -15,3 +15,22 @@ func BinarySearch(nums []int, target int) int {
 	}
 	return -1
 }
+
+/*
+搜索插入位置
+https://leetcode-cn.com/problems/search-insert-position/
+*/
+func searchInsert(nums []int, target int) int {
+	l, r := 0, len(nums)-1
+	idx := len(nums)
+	for l <= r {
+		mid := l + (r-l)/2
+		if nums[mid] >= target {
+			r = mid - 1
+			idx = mid
+		} else {
+			l = mid + 1
+		}
+	}
+	return idx
+}
--- algorithm/basic/sort.go
+++ algorithm/basic/sort.go
@@ -66,19 +66,3 @@ func partition(arr []int, left, right int) int {
 	arr[i], arr[right] = arr[right], arr[i]
 	return i
 }
-
-func BinarySearch(nums []int, target int) int {
-	l, r := 0, len(nums)-1
-	for l <= r {
-		mid := (r-l)/2 + l
-		if nums[mid] == target {
-			return mid
-		}
-		if nums[mid] > target {
-			r = mid - 1
-		} else {
-			l = mid + 1
-		}
-	}
-	return -1
-}
